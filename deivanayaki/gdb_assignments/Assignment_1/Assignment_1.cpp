18d2a98f91ca8114a2505bdda9da612d32a4f6e8
Deivanayaki-S <deivanayaki.sankaralingam@multicorewareinc.com>
2025-01-15 21:01:00 +0530

    1st Assignment question completed

--- deivanayaki/gdb_assignments/Assignment_1/Assignment_1.cpp
+++ deivanayaki/gdb_assignments/Assignment_1/Assignment_1.cpp
@@ -21,34 +21,25 @@ vector<pair<long, long>> sortWithFrequency(vector<long> v1){
     vector<pair<long, long>> v2;
     int i = 0;
     
-    sort(v1.begin()+1, v1.end());
+    sort(v1.begin(), v1.end());  // Include 0th index also for sorting
 
     while (i < v1.size()) {
-        pair<long,long> p;
-        if (v2.size() == 0) {
+        pair<long,long> p;   // Remove redundant codes
+        if (v2.size() == 0 || v2[v2.size()-1].first != v1[i]) {
             p.first = v1[i];
             p.second = count(v1.begin(), v1.end(), v1[i]);
             v2.push_back(p);
-            i++;
-            continue;
-        } else if (v2[v2.size()-1].first != v1[i]) {
-            p.first = v1[i];
-            p.second = count(v1.begin(), v1.end(), v1[i]);
-            v2.push_back(p);
-            i++;
-            continue;
         }
         i++;
     }
-    sort(v2.begin()+1, v2.end(),  comparecount);
+    sort(v2.begin(), v2.end(),  comparecount);  // Include 0th index also for sorting
     return v2;
 }
 
 int main() {
     long a[] = {6,5,7,5,5,6};
     vector<long> v1(a,a+6);
-    vector<pair<long, long>> v2 = sortWithFrequency(v1);
-    vector<pair<long, long>>::iterator it = v2.begin();
+    vector<pair<long, long>> v2 = sortWithFrequency(v1); // Remove unused line
     for (int i =0; i != v2.size(); i++) {
         cout << v2[i].first << " " << v2[i].second << std::endl;
     }
