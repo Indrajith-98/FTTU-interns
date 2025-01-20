6fd5f02b8ba77e764a00a375f6b0692c4892f292
Pravin P B <pravin.boopathi@multicorewareinc.com>
2025-01-20 10:17:49 +0530

    change file structure

--- pravin/cpp_assignments/src/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include <ctime>
-
-int main() {
-    std::time_t currentTime = std::time(nullptr);
-    std::cout << "Current time: " << std::ctime(&currentTime);
-    return 0;
-}
