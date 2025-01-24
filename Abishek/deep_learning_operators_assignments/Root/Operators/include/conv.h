6f332a175f42e4e7bd489c362d6327580f29bcfb
Abishek R <redme.abi04123@gmail.com>
2025-01-24 18:33:14 +0530

    add the src and header files for various deep learning operators (Conv, BatchNorm, Dense and MaxPool)

---
+++ Abishek/deep_learning_operators_assignments/Root/Operators/include/conv.h
@@ -0,0 +1,14 @@
+#ifndef CONV2D_H
+#define CONV2D_H
+
+#include <vector>
+#include <array>
+#include <string>
+
+void conv2d(const std::vector<float>& input, const std::vector<float>& kernel,
+            const std::vector<float>& bias, std::vector<float>& output,
+            const std::array<int, 4>& input_shape, const std::array<int, 4>& output_shape,
+            const std::array<int, 2>& kernel_size, const std::array<int, 2>& strides,
+            const std::string& padding, const std::string& layer_name);
+
+#endif // CONV2D_H
