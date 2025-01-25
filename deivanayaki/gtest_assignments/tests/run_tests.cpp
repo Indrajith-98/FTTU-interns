7b18e83f8c5741c68882c6546e8b5fbcd9e76587
Deivanayaki-S <deivanayaki.sankaralingam@multicorewareinc.com>
2025-01-25 22:59:58 +0530

    run test main updated

--- deivanayaki/gtest_assignments/tests/run_tests.cpp
+++ deivanayaki/gtest_assignments/tests/run_tests.cpp
@@ -5,20 +5,3 @@ int main(int argc, char **argv)
     :: testing :: InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
-
-/*
-CMDS : gcov CMakeFiles/MathLib.dir/src/my_math.cpp.gcno
-
-LCOV CMDS :
-
-cd CMakeFiles/MathLib.dir/src
-
-lcov --capture --directory . --output-file coverage.info
-
-genhtml coverage.info --output-directory out
-
-cd out
-
-explorer index.html
-
-*/ 
