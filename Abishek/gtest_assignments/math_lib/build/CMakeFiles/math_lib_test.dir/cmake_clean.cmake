308fa72c531e20b47d5a9b244902864d1309906e
Abishek R <redme.abi04123@gmail.com>
2025-01-26 22:42:42 +0530

    add gtest assignment files

---
+++ Abishek/gtest_assignments/math_lib/build/CMakeFiles/math_lib_test.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/math_lib_test.dir/test/test_my_math.cpp.obj"
+  "CMakeFiles/math_lib_test.dir/test/test_my_math.cpp.obj.d"
+  "libmath_lib_test.dll.a"
+  "math_lib_test.exe"
+  "math_lib_test.exe.manifest"
+  "math_lib_test.pdb"
+  "math_lib_test[1]_tests.cmake"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/math_lib_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Abishek/gtest_assignments/math_lib/build/googletest/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: F:/MultiCoreWare/Submissions/FTTU-interns/Abishek/gtest_assignments/math_lib/googletest/googletest
+# Build directory: F:/MultiCoreWare/Submissions/FTTU-interns/Abishek/gtest_assignments/math_lib/build/googletest/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
