b46cea8257c7024b6079e6d46e5b6e944822b97c
Pratheesh <pratheeshwaran04@gmail.com>
2025-01-27 16:58:41 +0530

    removed gtest files

--- Pratheesh/gtest_assignments/build/googletest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: F:/MCW/training assignments/gtest_assignments/googletest
-# Build directory: F:/MCW/training assignments/gtest_assignments/build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
