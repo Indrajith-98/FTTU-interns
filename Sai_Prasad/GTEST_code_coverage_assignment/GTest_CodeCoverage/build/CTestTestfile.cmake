753c49c5b795648041178942019eae475be727fe
Sai-Prasad-McW <sai.prasad@multicorewareinc.com>
2025-01-27 15:14:03 +0530

    GTest and code coverage assignment uploaded

---
+++ Sai_Prasad/GTEST_code_coverage_assignment/GTest_CodeCoverage/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /mnt/d/gtest_codecoverage
+# Build directory: /mnt/d/gtest_codecoverage/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(MyMathTest "/mnt/d/gtest_codecoverage/build/runTests")
+set_tests_properties(MyMathTest PROPERTIES  _BACKTRACE_TRIPLES "/mnt/d/gtest_codecoverage/CMakeLists.txt;34;add_test;/mnt/d/gtest_codecoverage/CMakeLists.txt;0;")
